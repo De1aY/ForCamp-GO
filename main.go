21b95aeca589aef2b660ce8ab8ea238aa7dcde4c
De1aY <6de1ay@gmail.com>
2017-03-06 19:02:00 +0300

    Added: default handler

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"fmt"
+)
 
+func defaultHandler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintf(w, "Hello!")
+}
+
+func main(){
+	Router := mux.NewRouter()
+	MainSite := Router.Host("forcamp.ga").Subrouter()
+	APISite := Router.Host("api.forcamp.ga").Subrouter()
+	APISite.HandleFunc("/", defaultHandler)
+	MainSite.HandleFunc("/", defaultHandler)
+	http.ListenAndServe(":80", Router)
+}
