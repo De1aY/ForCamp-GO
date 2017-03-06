68220bcdc95894c2bb42cbbf66cf5f452364322d
De1aY <6de1ay@gmail.com>
2017-03-06 22:21:35 +0300

    bug fixes

--- main.go
+++ main.go
@@ -12,11 +12,14 @@ func defaultHandler(w http.ResponseWriter, r *http.Request)  {
 
 func main(){
 	Router := mux.NewRouter()
+	WWWSite := Router.Host("www.forcamp.ga").Subrouter()
 	MainSite := Router.Host("forcamp.ga").Subrouter()
-	MainSite.HandleFunc("/", defaultHandler)
 	APISite := Router.Host("api.forcamp.ga").Subrouter()
+	MainSite.HandleFunc("/", defaultHandler)
 	APISite.HandleFunc("/", defaultHandler)
+	WWWSite.HandleFunc("/", defaultHandler)
 	http.ListenAndServe(":80", Router)
-	http.ListenAndServeTLS(":443", "./conf/tls/apiforcamp.pem", "./conf/tls/apiforcamp_key.pem", Router)
+	http.ListenAndServeTLS(":443", "./conf/tls/apiforcamp.pem", "./conf/tls/apiforcamp_key.pem", APISite)
 	http.ListenAndServeTLS(":443", "./conf/tls/forcamp.pem", "./conf/tls/forcamp_key.pem", MainSite)
+	http.ListenAndServeTLS(":443", "./conf/tls/wwwforcamp.pem", "./conf/tls/wwwforcamp_key.pem", WWWSite)
 }
