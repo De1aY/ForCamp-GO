6c83587c85575be0f920359b48bdb431bb210a48
De1aY <6de1ay@gmail.com>
2017-04-26 15:37:25 +0300

    Small fixes

--- main.go
+++ main.go
@@ -79,6 +79,6 @@ func main() {
 	src.Connection = src.Connect()
 
 	// Server
-	go http.ListenAndServe(conf.SERVER_PORT, Router)
-	handlers.HandleTLS(Router)
+	http.ListenAndServe(conf.SERVER_PORT, Router)
+	go handlers.HandleTLS(Router)
 }
