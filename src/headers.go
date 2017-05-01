0ba34af90720e82dde3e189ae8b564d1b50d2a07
Nikita De1aY Ivanov <6de1ay@gmail.com>
2017-05-01 12:44:03 +0300

    Bug fix

--- src/headers.go
+++ src/headers.go
@@ -29,7 +29,7 @@ SetHeaders_Main writes security HTTP-headers (X-XSS-Protection, Referrer-Policy,
  */
 func SetHeaders_Main(w http.ResponseWriter){
 	w.Header().Set("Access-Control-Allow-Method", "GET")
-	w.Header().Set("Content-Security-Policy", "default-src 'self' https://api.forcamp.ga; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com/; img-src 'self' data:; script-src 'unsafe-eval' 'self' https://cdnjs.cloudflare.com 'unsafe-inline'; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com")
+	w.Header().Set("Content-Security-Policy", "default-src 'self' https://api.forcamp.ga; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com/; img-src 'self' data:; script-src 'unsafe-eval' 'self' https://www.google-analytics.com/analytics.js https://cdnjs.cloudflare.com 'unsafe-inline'; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com")
 	w.Header().Set("X-XSS-Protection", "1; mode=block")
 	w.Header().Set("X-Frame-Options", "SAMEORIGIN")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
