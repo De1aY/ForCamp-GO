3026edf1c57cda209cbe95a84245b51ced99d87d
Nikita Ivanov <6de1ay@gmail.com>
2018-12-05 19:23:03 +0300

    Update addresses

--- src/handlers/exitHandler.go
+++ src/handlers/exitHandler.go
@@ -12,13 +12,13 @@ func ExitHandler(w http.ResponseWriter, r *http.Request){
 	if r.TLS != nil {
 		if r.Method == http.MethodGet{
 			w.Header().Set("Set-Cookie", "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT")
-			http.Redirect(w, r, "https://forcamp.ga", http.StatusTemporaryRedirect)
+			http.Redirect(w, r, "https://" + conf.MAIN_SITE_DOMAIN, http.StatusTemporaryRedirect)
 		} else {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			conf.ErrMethodNotAllowed.Print(w)
 		}
 	} else {
-		http.Redirect(w, r, "https://forcamp.ga/exit", http.StatusTemporaryRedirect)
+		http.Redirect(w, r, "https://" + conf.MAIN_SITE_DOMAIN + "/exit", http.StatusTemporaryRedirect)
 	}
 }
 
