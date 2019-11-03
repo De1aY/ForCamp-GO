a54bc49ff3e4c6d4dd5da082c4a4f1978d107914
Nikita Ivanov <de1ay@nullteam.info>
2019-11-04 02:18:13 +0300

    Update: tls certs&keys parts

--- conf/tls.go
+++ conf/tls.go
@@ -7,12 +7,12 @@ package conf
 // Paths for TLS certificates and keys
 const (
 	// Domain: "main"
-	MAIN_SITE_TLS     = "./conf/tls/wplay_tls.pem"
-	MAIN_SITE_TLS_KEY = "./conf/tls/wplay_tls_key.pem"
+	MAIN_SITE_TLS     = "./conf/tls/wplay.pem"
+	MAIN_SITE_TLS_KEY = "./conf/tls/wplay_key.pem"
 	/* Domain: "www.forcamp.nullteam.info"
 	WWW_MAIN_SITE_TLS = "./conf/tls/wwwforcamp.pem"
 	WWW_MAIN_SITE_TLS_KEY = "./conf/tls/wwwforcamp_key.pem" */
 	// Domain: "api"
-	API_SITE_TLS     = "./conf/tls/api_wplay_tls.pem"
-	API_SITE_TLS_KEY = "./conf/tls/api_wplay_tls_key.pem"
+	API_SITE_TLS     = "./conf/tls/api_wplay.pem"
+	API_SITE_TLS_KEY = "./conf/tls/api_wplay_key.pem"
 )
